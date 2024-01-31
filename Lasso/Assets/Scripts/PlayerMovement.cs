dacc1f93f33edc0516b8a2aa6fe896c34be1fc12
Arthur Upfield <arthurupfield@outlook.com>
2024-01-30 19:06:17 -0500

    Run animation
    Need to fix the character flip, setting the scale of the run animation manually broke the script that sets the flip based on direction by modifying the scale

--- Lasso/Assets/Scripts/PlayerMovement.cs
+++ Lasso/Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,41 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f; // Default values, change in Unity
     [SerializeField] private float jump = 5f;
     private Rigidbody2D body;
+    private Animator anim;
 
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
-        // Getting the Rigidbody2D component from the game object
+        // Grab references from game object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         // Setting the velocity of the Rigidbody2D component to the input axis on the X axis
-        body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Flipping the sprite based on the input axis
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Setting the velocity of the Rigidbody2D component to the input axis on the Y axis
             body.velocity = new Vector2(body.velocity.x, jump);
         }
+
+        // Setting the animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+
     }
 }
