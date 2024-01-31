c5404f9edff584dfe7dde4bb06380c39d67fd3e8
Arthur Upfield <arthurupfield@outlook.com>
2024-01-30 19:27:45 -0500

    Fixed weird scale thing

--- Lasso/Assets/Scripts/PlayerMovement.cs
+++ Lasso/Assets/Scripts/PlayerMovement.cs
@@ -28,11 +28,11 @@ public class PlayerMovement : MonoBehaviour
         // Flipping the sprite based on the input axis
         if (horizontalInput > 0.01f)
         {
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(6, 6, 1);
         }
         else if (horizontalInput < -0.01f)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-6, 6, 1);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
