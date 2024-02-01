979f11b12049d701a7f8b4ec21b524e6e109fd26
Dupree <143921581+doitmann@users.noreply.github.com>
2024-02-01 03:28:39 -0500

    Patrol
    Basic patrol for enemy...

---
+++ Lasso/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isRunning", true);
+    }
+
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if ((Vector2.Distance(transform.position, currentPoint.position) < 1f) && (currentPoint == pointB.transform))
+        {
+            flip();
+            currentPoint = pointA.transform;
+        }
+
+        if ((Vector2.Distance(transform.position, currentPoint.position) < 1f) && (currentPoint == pointA.transform))
+        {
+            flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);  
+    }
+}
