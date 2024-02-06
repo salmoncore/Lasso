77d0127d7711d626d0573ae8ad7324c998ff5031
Arthur Upfield <arthurupfield@outlook.com>
2024-02-06 15:39:43 -0500

    Attack animation WIP
    Going to rip up the current animation loop, wish me luck

---
+++ Lasso/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+	}
+
+	private void Update()
+	{
+        if (Input.GetKeyDown(";") && (cooldownTimer > attackCooldown) && playerMovement.canAttack())
+        {
+            Attack();
+        }
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+    }
+}
