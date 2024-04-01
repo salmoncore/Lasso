8d85ac6ca19c53c10de56ed88f7fa8b9686b22fc
Arthur Upfield <arthurupfield@outlook.com>
2024-04-01 17:31:01 -0400

    Cleaning up lol

--- Lasso/Assets/Scripts/BulletController.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BulletController : MonoBehaviour
-{
-    // After colliding with anything, change the bullet's tag to "
-}
